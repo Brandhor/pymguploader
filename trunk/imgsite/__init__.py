926a6ecde667c25344ac23b6e1a659d30ac1e7a5
brandon.ml <brandon.ml@317b0c32-7841-0410-8d08-bb87ba2ae35e>
2011-02-14 23:37:15 +0000

    remove uploadgeek.nl add radikal.ruk

--- trunk/imgsite/__init__.py
+++ trunk/imgsite/__init__.py
@@ -2,7 +2,7 @@
 #from imagecross import Imagecross
 from imageshack import Imageshack
 from uploadgeekcom import UploadgeekCom
-from uploadgeeknl import UploadgeekNl
+#from uploadgeeknl import UploadgeekNl
 #captcha
 #from tinypic import Tinypic
 from imagebam import Imagebam
@@ -14,3 +14,4 @@ from upmyphoto import Upmyphoto
 from freeimagehosting import Freeimagehosting
 from kb250 import KB250
 from simplestimhost import SimplestImHost
+from radikal import Radikal
